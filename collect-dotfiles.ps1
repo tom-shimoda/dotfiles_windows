ce893a44623fbd5fa545ec7236ba86b0c9e6a10d
tom-shimoda <shimoda0204@gmail.com>
2023-04-30 18:20:17 +0900

    edit

--- collect-dotfiles.ps1
+++ collect-dotfiles.ps1
@@ -1,5 +1,5 @@
 Copy-Item -Recurse -Force ~\AppData\Local\nvim nvim 
 
-Copy-Item -Recurse -Force C:\Users\%username%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 Microsoft.PowerShell_profile.ps1
+Copy-Item -Recurse -Force ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 Microsoft.PowerShell_profile.ps1
 
-Copy-Item -Recurse -Force C:\Users\%username%\AppData\Local\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json settings.json
+Copy-Item -Recurse -Force ~\AppData\Local\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json settings.json
--- scatter-dotfiles.ps1
+++ scatter-dotfiles.ps1
@@ -6,6 +6,6 @@ Copy-Item -Recurse -Force nvim ~\AppData\Local\nvim
 if (!(Test-Path ~\Documents\Powershell)) {
     mkdir ~\Documents\Powershell
 }
-Copy-Item -Recurse -Force Microsoft.PowerShell_profile.ps1 C:\Users\%username%\Documents\PowerShell
+Copy-Item -Recurse -Force Microsoft.PowerShell_profile.ps1 ~\Documents\PowerShell
 
-Copy-Item -Recurse -Force settings.json C:\Users\%username%\AppData\Local\Packages\Microsoft.WindowsTerminal_*\LocalState
+Copy-Item -Recurse -Force settings.json ~\AppData\Local\Packages\Microsoft.WindowsTerminal_*\LocalState
