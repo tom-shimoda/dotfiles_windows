2b50e3227c61d011ea56006d1a49e1fee340f781
tom-shimoda <shimoda0204@gmail.com>
2023-10-07 20:23:36 +0900

    edit

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -54,3 +54,29 @@ function compVideoFull {
     ffmpeg -i input.mp4 -c:a libopus -c:v libx265 -crf 31 -r 29.97 -tag:v hvc1 output.mp4
 }
 
+function youtubeDL_Audio {
+    yt-dlp $Args[0] -i -x --audio-format best --audio-quality 0 -o "C:\Users\Shimo\Documents\Youtube\Audio\%(title)s.%(ext)s"
+}
+
+function youtubeDL_Audio_Playlist {
+    $date = Get-Date -Format "yyyy_MM_dd"
+    yt-dlp $Args[0] -i -x --audio-format best --audio-quality 0 -o "C:\Users\Shimo\Documents\Youtube\Audio_playlist\%(playlist_title)s\$date\%(playlist_index)s_%(title)s.%(ext)s"
+}
+
+# 参考: https://hchch.net/youtube-dl-command-2021/
+function youtubeDL_Movie {
+    yt-dlp $Args[0] -i -f bestvideo+bestaudio/best -o "C:\Users\Shimo\Documents\Youtube\Movie\%(title)s - %(channel)s.%(ext)s" --add-metadata --embed-thumbnail --merge-output-format mkv -N 10
+}
+
+function youtubeDL_Movie_Playlist {
+    yt-dlp $Args[0] -i -f bestvideo+bestaudio/best -o "C:\Users\Shimo\Documents\Youtube\Movie_playlist\%(playlist_title)s\%(title)s.%(ext)s" --add-metadata --embed-thumbnail --merge-output-format mkv -N 10
+}
+
+function youtubeDL_NicoNico {
+    cd C:\Users\Shimo\Documents\github\youtube-dl\
+    python -m youtube_dl $Args[0]
+    start .
+    cd C:\Users\Shimo\Documents\Youtube\Movie
+    start .
+}
+
