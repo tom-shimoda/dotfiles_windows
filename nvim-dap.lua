22671c6450ee99974c987a480bd0aa73e251844d
tom-shimoda <tom-shimoda@example.com>
2023-10-11 11:43:30 +0900

    Add nvim-dap-virtual-text

--- nvim-dap.lua
+++ nvim-dap.lua
@@ -1,3 +1,4 @@
+-- >>> mappings
 local function map(mode, lhs, rhs, opts)
 	local options = {noremap = true}
     if opts then options = vim.tbl_extend('force', options, opts) end
@@ -19,7 +20,7 @@ map("n", "<leader><leader>df", ":lua require'dapui'.eval()<CR>", { silent = true
 
 -- dap-go key map
 map("n", "<leader>td", ":lua require'dap-go'.debug_test()<CR>", { silent = true })
-
+-- <<< mappings
 
 -- >>> dap-uiの設定
 require("dapui").setup({
@@ -163,11 +164,52 @@ dap.configurations.go = {
 }
 -- <<< dap-goの設定 ここまで
 
+-- >>> dap-virtual-textの設定 (インライン表示はnvim 0.10~みたいで現段階では使えない)
+require("nvim-dap-virtual-text").setup {
+    enabled = true,                        -- enable this plugin (the default)
+    enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+    highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+    highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+    show_stop_reason = true,               -- show stop reason when stopped for exceptions
+    commented = false,                     -- prefix virtual text with comment string
+    only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
+    all_references = false,                -- show virtual text on all all references of the variable (not only definitions)
+    clear_on_continue = false,             -- clear virtual text on "continue" (might cause flickering when stepping)
+    --- A callback that determines how a variable is displayed or whether it should be omitted
+    --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+    --- @param buf number
+    --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+    --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+    --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
+    --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+    display_callback = function(variable, buf, stackframe, node, options)
+      if options.virt_text_pos == 'inline' then
+        return ' = ' .. variable.value
+      else
+        return variable.name .. ' = ' .. variable.value
+      end
+    end,
+    -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
+    -- virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
+    virt_text_pos = 'eol',
+
+    -- experimental features:
+    all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+    virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
+    virt_text_win_col = nil                -- position the virtual text at a fixed window column (starting from the first text column) ,
+                                           -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+}
+-- <<< dap-virtual-textの設定 ここまで
+
+
+-- >>>その他
+-- ブレークポイントアイコン変更
+vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''})
+
 -- デバッグ開始時に自動的にデバッグUI表示
 require'dap'.listeners.before['event_initialized']['custom'] = function(session, body)
     require'dapui'.open()
 end
-
 -- デバッグ終了時に自動的にデバッグUI非表示
 -- require'dap'.listeners.before['event_terminated']['custom'] = function(session, body)
 --     require'dapui'.close()
